1302043e17ef8cdc0efa61871a6ca09f69b75ccb
Santiago <freddysgalan@gmail.com>
2025-05-08 16:06:26 -0500

    Actualización de Ngrok URL: https://eb8c-181-129-180-130.ngrok-free.app

--- empleado.py
+++ empleado.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from datetime import date, time
 
-API_URL = "https://e05d-181-129-180-130.ngrok-free.app"
+API_URL = "https://eb8c-181-129-180-130.ngrok-free.app"
 
 st.title("📄 Solicitud de Permiso de Salida")
 
