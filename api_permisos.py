e4c5a25a214456af8ff4b5c0cec140d92ce42708
Santiago <freddysgalan@gmail.com>
2025-05-08 15:45:12 -0500

    Actualización de Ngrok URL: https://e05d-181-129-180-130.ngrok-free.app

--- api_permisos.py
+++ api_permisos.py
@@ -170,5 +170,5 @@ def solicitudes_pendientes(jefe_inmediato):
             })
     return jsonify(results)
 
-if __name__ == "__main__":
-    app.run(port=5002, debug=False)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
